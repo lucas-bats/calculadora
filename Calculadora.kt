2c759be833d0e45322b3402acdc382179ce24c72
Lucas Batista <86981731+lucas-bats@users.noreply.github.com>
2021-07-10 23:33:36 -0300

    Add files via upload

---
+++ Calculadora.kt
@@ -0,0 +1,19 @@
+fun main( ){
+    var resultado:Int
+    var num1:Int
+    var num2:Int
+    var op1:Int
+    println("Bem vindo(a) a Calculadora!")
+
+    println("Entre o primeiro número: ")
+    num1 = Integer.valueOf(readLine())
+    println("Entre com o segundo número: ")
+    num2 = Integer.valueOf(readLine())
+    println("Qual operação deseja fazer com esses números? 1 (Adição), 2 (Subtração), 3 (Multiplicação) ou 4 (Divisão?) ")
+    op1 = Integer.valueOf(readLine())
+    if(op1 == 2) {
+        resultado = num1 - num2
+        println(resultado)
+    }
+    }
+
