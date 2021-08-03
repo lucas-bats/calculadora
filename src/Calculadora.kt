85d1919bdb9eb9633364a77f2d37d104084f1826
lucas-bats <lucaspaesbatista1@gmail.com>
2021-08-02 21:38:16 -0300

    Divisão

--- src/Calculadora.kt
+++ src/Calculadora.kt
@@ -9,21 +9,24 @@ fun main( ){
     num1 = Integer.valueOf(readLine())
     println("Entre com o segundo número: ")
     num2 = Integer.valueOf(readLine())
-    println("Qual operação deseja fazer com esses números? 1 (Adição), 2 (Subtração), 3 (Multiplicação) ou 4 (Divisão?) ")
+    println("Qual operação deseja fazer com esses números? 1 (Adição), 2 (Subtração), 3 (Multiplicação) ou 4 (Divisão)? ")
     op1 = Integer.valueOf(readLine())
     if(op1 == 2) {
-        resultado = num1 - num2
-    }else{
-            if(op1==1){
-            resultado = num1+num2
-          }else{
-              if(op1==3){
-              resultado = num1*num2
-              }
-            }
+        resultado = num1 - num2}
+    else{
+        if(op1==1){
+            resultado = num1+num2}
+        else{
+            if(op1==3){
+                resultado = num1*num2 }
+            else{
+            if(op1==4){
+                resultado = num1/num2
+            }}
+        }
     }
         println(resultado)
-        println("Olá Mundo")
+
     }
     
 
